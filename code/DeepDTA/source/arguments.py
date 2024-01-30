534f445029509c2744f45190b03754b18ded3f8f
Kusal Debnath <debnathk1997@gmail.com>
2024-01-30 16:54:30 -0500

    Code modified

--- code/DeepDTA/source/arguments.py
+++ code/DeepDTA/source/arguments.py
@@ -70,7 +70,7 @@ def argparser():
   parser.add_argument(
       '--dataset_path',
       type=str,
-      default='/data/kiba/',
+      default='./data/kiba/', #code\DeepDTA\source\data\kiba, 
       help='Directory for input data.'
   )
   parser.add_argument(
