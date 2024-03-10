528b0df5e8a2d9c70fec1ab52685a17642ccf295
Kusal Debnath <debnathk1997@gmail.com>
2024-03-10 16:08:27 -0400

    Code added

--- results/deepPurpose/dti/run_deepPurpose_dti_mpnn_cnn_kiba.sh
+++ results/deepPurpose/dti/run_deepPurpose_dti_mpnn_cnn_kiba.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
-#SBATCH --job-name=graphDTA
+#SBATCH --job-name=deepPurpose
 #SBATCH --output output_deepPurpose_dti_kiba.log
 #SBATCH --partition gpu
-#SBATCH --nodes 1
-#SBATCH --ntasks 1
-#SBATCH --cpus-per-task 1
 #SBATCH --mem=128G           # Set the memory required per node
-#SBATCH --gres=gpu:1    # Request 1 GPU
-#SBATCH --time=72:00:00    # Set the maximum time your job will run (hh:mm:ss)
+#SBATCH --gres=gpu:1         # Request 1 GPU
 
 echo "Date"
 date
@@ -15,23 +11,13 @@ date
 start_time=$(date +%s)
 
 # Load the necessary modules
-# module purge
-module load cuda/12.3   # Change this to the appropriate CUDA version
-# module load cudnn/8.0.4   # Change this to the appropriate cuDNN version
-# module load anaconda/2020.11  # Change this to the appropriate Anaconda version
+module load cuda/12.3        # Change this to the appropriate CUDA version
 
 # pip install --user DeepPurpose
 # pip install --user git+https://github.com/bp-kelley/descriptastorus
 
-# Activate your Python environment
-# source activate your_conda_environment  # Change this to the name of your Conda environment
-
-# Navigate to the directory containing your Python script
-# cd /path/to/your/python/script
-
 # Run your Python script
-# python create_data.py
-python run_dti_kiba.py
+python run_deepPurpose_dti_mpnn_cnn_kiba.py
 
 end_time=$(date +%s)
 elapsed_time=$((end_time - start_time))
