ed368f08b55b622d500c96345ddee06a32d5e567
debnathk <debnathk1997@gmail.com>
2024-03-16 14:25:48 -0400

    Add code

--- results/deepPurpose/dti/evaluate_deepPurpose_dti_cnn_cnn_davis.py
+++ results/deepPurpose/dti/evaluate_deepPurpose_dti_cnn_cnn_davis.py
@@ -4,7 +4,7 @@ import warnings
 from lifelines.utils import concordance_index
 warnings.filterwarnings("ignore")
 
-X_drugs, X_targets, y = dataset.load_process_DAVIS(path = './data', binary = True,
+X_drugs, X_targets, y = dataset.load_process_DAVIS(path = './data', binary = False,
                                                   convert_to_log = True, # Enable for DAVIS dataset
                                                    threshold = 30)
 print('Drug 1: ' + X_drugs[0])
@@ -12,7 +12,7 @@ print('Target 1: ' + X_targets[0])
 print('Score 1: ' + str(y[0]))
 
 # DTI prediction framework
-drug_encoding, target_encoding = 'MPNN', 'CNN'
+drug_encoding, target_encoding = 'CNN', 'CNN'
 #drug_encoding, target_encoding = 'Morgan', 'Conjoint_triad'
 
 # Data preparation
@@ -30,8 +30,8 @@ config = utils.generate_config(drug_encoding = drug_encoding,
                          LR = 0.001,
                          batch_size = 128,
                          hidden_dim_drug = 128,
-                         mpnn_hidden_size = 128,
-                         mpnn_depth = 3,
+                         cnn_drug_filters = [32,64,96],
+                         cnn_drug_kernels = [4,8,12],
                          cnn_target_filters = [32,64,96],
                          cnn_target_kernels = [4,8,12]
                         )
@@ -46,7 +46,7 @@ model.train(train, val, test)
 # Load dataset
 import pandas as pd
 
-test_data = pd.read_csv('../../../cancer_datasets/breast_cancer/test_bin.csv')
+test_data = pd.read_csv('../../../cancer_datasets/breast_cancer/test_reg.csv')
 test_data
 
 # Predict
@@ -54,8 +54,8 @@ y_pred = []
 for i in range(len(test_data)):
   X_drug = [test_data['canonical_smiles'][i]]
   X_target = [test_data['target'][i]]
-  # y = [test_data['pIC50'][i]]
-  y = [test_data['class_label'][i]]
+  y = [test_data['pIC50'][i]]
+  # y = [test_data['class_label'][i]]
   X_pred = utils.data_process(X_drug, X_target, y,
                                   drug_encoding, target_encoding,
                                   split_method='no_split')
@@ -68,23 +68,23 @@ import numpy as np
 from sklearn.metrics import mean_squared_error
 
 # Assuming y_true and y_pred are your data
-# y_true = np.array(test_data['pIC50'])
-y_true = np.array(test_data['class_label'])
+y_true = np.array(test_data['pIC50'])
+# y_true = np.array(test_data['class_label'])
 y_pred = np.squeeze(y_pred)
 
-# # Calculate MSE
-# mse = mean_squared_error(y_true, y_pred)
-# print(f'Mean Squared Error: {mse}')
+# Calculate MSE
+mse = mean_squared_error(y_true, y_pred)
+print(f'Mean Squared Error: {mse}')
 
-# # Calculate CI
-# ci = concordance_index(y_true, y_pred)
-# print(f'Concordance Index: {ci}')
+# Calculate CI
+ci = concordance_index(y_true, y_pred)
+print(f'Concordance Index: {ci}')
 
-# Calculate AUROC
-utils.roc_curve(y_pred, y_true, 'deepPurpose_auroc_mpnn_cnn.png')
+# # Calculate AUROC
+# utils.roc_curve(y_pred, y_true, 'deepPurpose_auroc_cnn_cnn.png')
 
-# Calculate AUPR
-utils.prauc_curve(y_pred, y_true, 'deepPurpose_aupr_mpnn_cnn.png')
+# # Calculate AUPR
+# utils.prauc_curve(y_pred, y_true, 'deepPurpose_aupr_cnn_cnn.png')
 
 pd.DataFrame(y_true).to_csv('y_true.csv', index=False, header=None)
 pd.DataFrame(y_pred).to_csv('y_pred.csv', index=False, header=None)
---
+++ results/deepPurpose/dti/evaluate_deepPurpose_dti_mpnn_cnn_davis_bin.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+#SBATCH --job-name=evaluate_deepPurpose_bin
+#SBATCH --output evaluate_deepPurpose_dti_mpnn_cnn_davis_50epoch_bin.log
+#SBATCH --error error_evaluate_deepPurpose_dti_mpnn_cnn_davis_bin.log
+#SBATCH --partition gpu
+#SBATCH --mem=64G      
+#SBATCH --gres=gpu:1
+
+echo "Date"
+date
+
+start_time=$(date +%s)
+
+# Load the necessary modules
+# module purge
+module load cuda/12.3   # Change this to the appropriate CUDA version
+# module load cudnn/8.0.4   # Change this to the appropriate cuDNN version
+# module load anaconda/2020.11  # Change this to the appropriate Anaconda version
+
+# pip install --user DeepPurpose --quiet
+# pip install --user git+https://github.com/bp-kelley/descriptastorus --quiet
+
+# Activate your Python environment
+source activate /home/debnathk/articles/.venv/bin/python
+
+# Run your Python script
+python evaluate_deepPurpose_dti_mpnn_cnn_davis.py
+
+end_time=$(date +%s)
+elapsed_time=$((end_time - start_time))
+echo "Elapsed time: $elapsed_time seconds"
